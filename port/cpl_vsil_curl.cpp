47339be2942b3a87f5c630bbc05c88e0c57ef5d1
gruan <mailruangong@gmail.com>
2026-04-01 19:00:24 +0800

    update curl to support onefs s3 api

--- port/cpl_vsil_curl.cpp
+++ port/cpl_vsil_curl.cpp
@@ -668,6 +668,10 @@ size_t VSICurlHandleWriteFunc( void *buffer, size_t count,
             {
                 psStruct->bFoundContentRange = true;
             }
+            else if( STARTS_WITH_CI(pszLine, "Content-Range:") )
+            {
+                psStruct->bFoundContentRange = true;
+            }
             else if( STARTS_WITH_CI(pszLine, "Date: ") )
             {
                 CPLString osDate = pszLine + strlen("Date: ");
@@ -1328,6 +1332,9 @@ retry:
                 if( pszContentRange == nullptr )
                     pszContentRange = strstr(sWriteFuncHeaderData.pBuffer,
                            "content-range: bytes ");
+                if( pszContentRange == nullptr )
+                    pszContentRange = strstr(sWriteFuncHeaderData.pBuffer,
+                           "Content-Range:bytes ");
                 if( pszContentRange )
                     pszContentRange = strchr(pszContentRange, '/');
                 if( pszContentRange )
@@ -1819,6 +1826,9 @@ retry:
         if( pszContentRange == nullptr )
             pszContentRange = strstr(sWriteFuncHeaderData.pBuffer,
                                      "content-range: bytes ");
+        if( pszContentRange == nullptr )
+            pszContentRange = strstr(sWriteFuncHeaderData.pBuffer,
+                                     "Content-Range:bytes ");
         if( pszContentRange )
         {
             char* pszEOL = strchr(pszContentRange, '\n');
@@ -2605,6 +2615,11 @@ int VSICurlHandle::ReadMultiRangeSingleGet(
             }
 
             if( STARTS_WITH_CI(pszNext, "Content-Range: bytes ") )
+            {
+                bExpectedRange = true; /* FIXME */
+            }
+
+            if( STARTS_WITH_CI(pszNext, "Content-Range:bytes ") )
             {
                 bExpectedRange = true; /* FIXME */
             }
